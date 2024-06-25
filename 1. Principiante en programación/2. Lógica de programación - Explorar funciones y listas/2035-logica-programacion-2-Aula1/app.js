9e0bc849db5dd1f2cc54dc2845b9a1ef4decdc47
Jonny <jonabeltran1122@gmail.com>
2024-06-25 15:39:37 -0600

    Comenzando la sección 4: Listas

--- 1. Principiante en programación/2. Lógica de programación - Explorar funciones y listas/2035-logica-programacion-2-Aula1/app.js
+++ 1. Principiante en programación/2. Lógica de programación - Explorar funciones y listas/2035-logica-programacion-2-Aula1/app.js
@@ -1,5 +1,7 @@
 //Definiendo variables
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto;
+let intentos;
+console.log(numeroSecreto);
 
 
 //Definiendo funciones
@@ -11,19 +13,41 @@ function asignarTextoElemento(elemento, texto){
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
     if(numeroDeUsuario === numeroSecreto){
-        asignarTextoElemento("p","Acerstaste el número secreto!!");        
+        asignarTextoElemento("p",`Acertaste el número en ${intentos} ${intentos === 1 ? "intento" : "intentos"}`);
+        document.getElementById("reiniciar").removeAttribute("disabled");        
     } else {
+        // El usuario no acertó el número
         if(numeroDeUsuario > numeroSecreto){
             asignarTextoElemento("p", "El número secreto es menor");
         } else {
             asignarTextoElemento("p", "El número secreto es mayor");
         }
+        intentos++;
+        limpiarCaja();
     }
     return;
+}
+function limpiarCaja(){
+    document.querySelector("#valorUsuario").value = "";
 }
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10) + 1;
 }
+function condicionesIniciales(){
+    asignarTextoElemento("h1","Juego del número secreto!");
+    asignarTextoElemento("p","Indica un número del 1 al 10");
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+function reiniciarJuego(){
+    //Limpiar la caja
+    limpiarCaja();
+    //Indicar mensaje de rango de números
+    //Generar nuevo número aleatorio
+    //Reiniciar el número de intentos
+    condicionesIniciales();
+    //Deshabilitar botón de nuevo juego
+    document.querySelector("#reiniciar").setAttribute("disabled","true");
+}
 
-asignarTextoElemento("h1","Juego del número secreto!");
-asignarTextoElemento("p","Indica un número del 1 al 10");
+condicionesIniciales();
